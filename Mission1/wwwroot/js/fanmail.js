0696cc74464aa79d539d5653146cf60cc899f973
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2022-01-07 10:11:16 -0700

    Initial check-in of module Mission1

---
+++ Mission1/wwwroot/js/fanmail.js
@@ -0,0 +1,23 @@
+﻿//One of them needs to be commented out //
+
+
+//HERE is the normal JavaScript//
+
+
+
+document.getElementById("btnSend").addEventListener('click', function () {
+    alert("From: " + document.getElementById("from").value +
+        ", Subject: " + document.getElementById("subject").value +
+        ", Message: " + document.getElementById("message").value);
+})
+
+
+
+
+//here is the jQuery shortcut//
+
+//$("#btnSend").click(function () {
+//    alert("From: " + $("#from").val() +
+//        ", Subject: " + $("#subject").val() +
+//        ", Message: " + $("#message").val());
+//})
